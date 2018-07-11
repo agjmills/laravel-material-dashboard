75fad8e264bb6d7f31a97a18d2fec1613cce615e
Alex Mills <alex.mills@drpgroup.com>
2018-07-11 11:32:39 +0100

    updated documentation

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace Agjmills\MaterialDashboard;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
-class ServiceProvider extends ServiceProvider
+class ServiceProvider extends BaseServiceProvider
 {
     public function boot(): void
     {
@@ -12,21 +12,14 @@ class ServiceProvider extends ServiceProvider
 
         $this->publishes(
             [
-                __DIR__ . '/assets' => public_path('/vendor/laravel-material-dashboard')
-            ],
-            'assets'
+                __DIR__ . '/assets' => public_path('/vendor/laravel-material-dashboard'),
+            ], 'assets'
         );
 
         $this->publishes(
             [
-                __DIR__ . '/config/laravel-material-dashboard.php' => config_path('/laravel-material-dashboard.php')
-            ],
-            'config'
+                __DIR__ . '/config/laravel-material-dashboard.php' => config_path('/laravel-material-dashboard.php'),
+            ], 'config'
         );
-    }
-
-    public function register(): void
-    {
-        //
     }
 }
