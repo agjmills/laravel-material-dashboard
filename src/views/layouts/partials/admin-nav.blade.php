a795b331cfc3b638efcc203dcbc778a6556353c8
Alex Mills <alex@asdfx.us>
2020-06-09 18:16:26 +0100

    missed array_get

--- src/views/layouts/partials/admin-nav.blade.php
+++ src/views/layouts/partials/admin-nav.blade.php
@@ -1,7 +1,7 @@
 @foreach(config('laravel-material-dashboard.menu') as $item)
     <ul class="nav">
         @include('MaterialDashboard::partials.nav-permissions', ['item' => $item])
-        @foreach(array_get($item, 'links', []) as $subItem)
+        @foreach(Illuminate\Support\Arr::get($item, 'links', []) as $subItem)
             @include('MaterialDashboard::partials.nav-permissions', ['item' => $subItem])
         @endforeach
     </ul>
